b9bf84133fe6b4eb2a476c342f298cbdcace9c35
jimvanzummeren <j.van.zummeren@gmail.com>
2013-10-22 15:19:43 +0200

    CSS

--- source/js/models/game.js
+++ source/js/models/game.js
@@ -17,7 +17,7 @@ define([
 	parse: function(resp, xhr) {
 
 	    resp.start_date = new Date(resp.start_time).toString('d MMMM yyyy HH:mm');
-      resp.start_day = new Date(resp.start_time).toString('dddd - d MMMM yyyy');
+      resp.start_day = new Date(resp.start_time).toString('dddd - MMMM d, yyyy');
       resp.start_time = new Date(resp.start_time).toString('HH:mm');
       
 	    return resp;
