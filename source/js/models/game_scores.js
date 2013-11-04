3ee9271feb8cc43d41eb38161873101b5eb1a137
Machiel-B <A.M.Banen@gmail.com>
2013-11-04 14:46:11 +0100

    Update game_scores.js
    You are not using this file

--- source/js/models/game_scores.js
+++ source/js/models/game_scores.js
@@ -1,24 +1,3 @@
 /*
-* Not used?
+* Not used? Yes it's not used
 */
-
-define([
-  'backbone'
-], function(Backbone){
-  var GameScores = Backbone.Model.extend({
-  id: null,
-  urlRoot: "/game_scores/",
-  fields: "[]",
-
-  url: function(){
-    if(this.get('id')){
-      return this.urlRoot + this.get('id') + "/";// + "?fields=" + encodeURIComponent(this.fields);
-    }else{
-      return this.urlRoot;
-    }
-  }
-
-  });
-  // Return the model for the module
-  return GameScores;
-});
